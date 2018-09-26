bf3823c9f7bedfeea5fa51f5ca4588c3d5853ad9
Jeff Killinger <jeffkillinger@gmail.com>
2018-09-26 11:43:56 -0500

    set up files and navbar component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import AppNavbar from './components/layout/AppNavbar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div class="container">
+            <h1>This is gonna be da big one</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
